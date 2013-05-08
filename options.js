891c976ff5f8087243d532b397e956af123c1957
s0snooley <l4dmobile@mail.ru>
2013-05-08 23:46:11 +0400

    Update options.js

--- options.js
+++ options.js
@@ -2,12 +2,29 @@ function loadOptions() {
 	var ioscompat = document.getElementById("fieldioscompat");
 	var value = localStorage["ioscompat"];
 	ioscompat.checked = (value == "true");
+	
+	var blacklist = document.getElementById("fieldblacklist");
+	var value2 = localStorage["blacklist"];
+	blacklist.value = value2;
+	
+	var emojisize = document.getElementById("fieldemojisize");
+	var value3 = localStorage["emojisize"];
+	emojisize.value = value3;
 }
 
 function saveOptions() {
 	var ioscompat = document.getElementById("fieldioscompat");
 	var value = ioscompat.checked;
 	localStorage["ioscompat"] = value;
+	
+	var blacklist = document.getElementById("fieldblacklist");
+	var value2 = blacklist.value;
+	localStorage["blacklist"] = value2;
+	
+	var emojisize = document.getElementById("fieldemojisize");
+	var value3 = emojisize.value;
+	localStorage["emojisize"] = value3;
+	
 	window.close();
 }
 
